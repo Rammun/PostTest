6d178ff1cc0ad38b2eec3009afe5558aaf232944
Ruslan Likhobaba <rl@payler.com>
2017-04-28 02:43:16 +0300

    [add] Добавил валидацию формы регистрации посылки.

--- src/PostTest.Core/Models/ParcelViewModel.cs
+++ src/PostTest.Core/Models/ParcelViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PostTest.Core.Models
 {
@@ -16,18 +12,17 @@ namespace PostTest.Core.Models
         public double Weight { get; set; }
 
         /// <summary>
+        /// Опись
+        /// </summary>
+        [Display(Name = "Опись")]
+        public string Inventory { get; set; }
+
+        /// <summary>
         /// ФИО получателя
         /// </summary>
         [Required]
         [Display(Name = "ФИО получателя")]
         public string RecipientFullName { get; set; }
-
-        /// <summary>
-        /// ФИО отправителя
-        /// </summary>
-        [Required]
-        [Display(Name = "ФИО отправителя")]
-        public string SenderFullName { get; set; }
 
         /// <summary>
         /// Адресс получателя
@@ -37,16 +32,17 @@ namespace PostTest.Core.Models
         public string RecipientAdress { get; set; }
 
         /// <summary>
+        /// ФИО отправителя
+        /// </summary>
+        [Required]
+        [Display(Name = "ФИО отправителя")]
+        public string SenderFullName { get; set; }
+        
+        /// <summary>
         /// Адресс отправителя
         /// </summary>
         [Required]
         [Display(Name = "Адрес отправителя")]
         public string SenderAdress { get; set; }
-
-        /// <summary>
-        /// Опись
-        /// </summary>
-        [Display(Name = "Опись")]
-        public string Inventory { get; set; }
     }
 }
