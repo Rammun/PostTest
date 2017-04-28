498afae4ca697cbc4054d593c8031c7f2358aeba
Ruslan Likhobaba <rl@payler.com>
2017-04-28 15:17:24 +0300

    [add] Перенес скрипт в site.js

--- src/PostTest.Core/wwwroot/js/site.js
+++ src/PostTest.Core/wwwroot/js/site.js
@@ -1,67 +1,69 @@
-﻿//"use strict";
+﻿"use strict";
 
-//searchRender();
+var baseUrl = "http://localhost:51359/";
 
-//$(document).ready(function () {
-//    $('#buttonSearch').click(searchRender);
-//});
+searchRender();
 
-//$('#formRegister').submit(function () {
-//    var validator = $("#formRegister").validate();
-//    if (validator.form()) {
-//        register();
-//        this.form.reset();
-//        searchRender();
-//    }
-//    return false;
-//});
+$(document).ready(function () {
+    $('#buttonSearch').click(searchRender);
+});
 
-//function searchRender() {
-//    $.ajax({
-//        url: '@Url.Action("ParcelSearch", "Home")',
-//        data: $('#formSearch').serialize(),
-//        type: 'GET',
-//        success: function (obj) {
-//            $('#searchresults').html(obj);
-//        },
-//        error: function (obj) {
-//            alert('Error!');
-//        }
-//    });
-//}
+$('#formRegister').submit(function () {
+    var validator = $("#formRegister").validate();
+    if (validator.form()) {
+        register();
+        this.form.reset();
+        searchRender();
+    }
+    return false;
+});
 
-//function register() {
-//    $.ajax({
-//        url: '@Url.Action("ParcelRegister", "Home")',
-//        data: getFormJson(),
-//        type: 'POST',
-//        contentType: 'application/json',
-//        dataType: 'json',
-//        success: function (obj) {
-//            $('#registerModal').modal("hide");
-//        },
-//        error: function (obj) {
-//            alert('Не валидные параметры!');
-//        }
-//    });
-//}
+function searchRender() {
+    $.ajax({
+        url: baseUrl + "Home/ParcelSearch",   //'@Url.Action("ParcelSearch", "Home")',
+        data: $('#formSearch').serialize(),
+        type: 'GET',
+        success: function (obj) {
+            $('#searchresults').html(obj);
+        },
+        error: function (obj) {
+            alert('Error!');
+        }
+    });
+}
 
-//function getFormJson() {
-//    var parcel = {
-//        weight: $('#parcel_weight').val(),
-//        inventory: $('#parcel_inventory').val(),
-//        recipient: {
-//            firstName: $('#recipient_firstName').val(),
-//            lastName: $('#recipient_lastName').val(),
-//            patronymic: $('#recipient_patronymic').val(),
-//            address: $('#recipient_address').val()
-//        },
-//        sender: {
-//            firstName: $('#sender_firstName').val(),
-//            lastName: $('#sender_lastName').val(),
-//            patronymic: $('#sender_patronymic').val(),
-//            address: $('#sender_address').val()
-//        }
-//    };
-//    return JSON.stringify(parcel);
-//}
+function register() {
+    $.ajax({
+        url: baseUrl + "Home/ParcelRegister",  // '@Url.Action("ParcelRegister", "Home")',
+        data: getFormJson(),
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function (obj) {
+            $('#registerModal').modal("hide");
+        },
+        error: function (obj) {
+            alert('Не валидные параметры!');
+        }
+    });
+}
+
+function getFormJson() {
+    var parcel = {
+        weight: $('#parcel_weight').val(),
+        inventory: $('#parcel_inventory').val(),
+        recipient: {
+            firstName: $('#recipient_firstName').val(),
+            lastName: $('#recipient_lastName').val(),
+            patronymic: $('#recipient_patronymic').val(),
+            address: $('#recipient_address').val()
+        },
+        sender: {
+            firstName: $('#sender_firstName').val(),
+            lastName: $('#sender_lastName').val(),
+            patronymic: $('#sender_patronymic').val(),
+            address: $('#sender_address').val()
+        }
+    };
+    return JSON.stringify(parcel);
+}
